c87dfcfe8118f7accd4e163563bf16d7157ea919
Ho <godqhr721@gmail.com>
2022-05-07 01:53:11 +0900

    Error corrected

--- arr_average.cpp
+++ arr_average.cpp
@@ -1,14 +1,13 @@
 #include <iostream>
 using namespace std;
 int main() {
-	int num,score,sum = 0;
+	int num,sum = 0;
 	cin >> num;
 	
 	int average[num-1];
 	for(int i =0;i < num; i++){
-		cin >> score;
-		average[i] = score;
-		sum += score;
+		cin >> average[i];
+		sum += average[i];
 	}
 	cout <<"학생들의 성적 평균은 "<< sum /num<<"점 입니다.";
 	
